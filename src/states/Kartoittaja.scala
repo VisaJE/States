df3935f8810674dfd96986788a6f4f3bbeb3fb31
Eemil Visakorpi <Eemil.Visakorpi@aalto.fi>
2018-03-05 11:53:41 +0200

    Kehitetty kartoittajan piirto-metodeja ja blokkeja.

--- src/states/Kartoittaja.scala
+++ src/states/Kartoittaja.scala
@@ -7,6 +7,7 @@ import javax.imageio.ImageIO
 import java.io.File
 import javax.swing._
 import java.awt.Image
+import java.awt.image.BufferedImage
 import scala.util.Random
 
 
@@ -23,12 +24,30 @@ class Kartoittaja(k: Kartta) {
       "kartat/laavaa.png",
       "kartat/kivi.png",
       "kartat/lätäkkö.png",
-      "kartat/puu.png"
-      )
+      "kartat/puu.png")
   
+      
+  private def haeKuva(l: Laitos) = {
+    val pth = l match {
+      case _: Pelto => peltop
+      case _: Kaivos => kaivosp
+      case _: Tehdas => tehdasp
+      case _ => throw AsetusVirhe(message = "Kuva puuttuu laitokselle " +
+          l.toString)
+    }
+    ImageIO.read(new File(pth))
+  }
+  
+  
+      
   
   // Kartta koostuu neliöistä, joihin voi sijoittua koristeita tai laitoksia.
-  private val blokit = Array.ofDim[Blokki](20, 20).map(_.map(_ =>new Blokki))
+  private val blokit = Array.ofDim[Blokki](20, 20)
+  for (i <- 0 to 19) {
+    for (j <- 0 to 19) {
+      blokit(j)(i) = new Blokki(j*100, i*100)
+    }
+  }
   
   
   // Reunat pois käytöstä
@@ -55,17 +74,37 @@ class Kartoittaja(k: Kartta) {
   
   
   def asetaLaitos(l: Laitos) = {
-    jokuBlokki.lisääLaitos(l)
+    jokuBlokki.lisääLaitos(l, haeKuva(l))
   }
   
   
+  // Lataillaan kartta ja lisätään ikonit
+  private def kartta = {
+    val tulos = ImageIO.read(new File(path))
+    val g2d = tulos.createGraphics()
+    blokit.foreach(_.foreach(b => b.ikoni.foreach(i => g2d.drawImage(i, b.xCoord, b.yCoord, null))))
+    ImageIO.write(tulos, "PNG", new File("pelikartta.png"))
+  }
   
-  private val kartta = ImageIO.read(new File(path))
-  private val icon = new ImageIcon(path)
-  private val miniIcon = kartta.getScaledInstance(100, 100, Image.SCALE_DEFAULT)
+  // private val icon = new ImageIcon(path)
+  private val miniIcon = ???//kartta.getScaledInstance(100, 100, Image.SCALE_DEFAULT)
   
   
-  def getKartta = new JLabel(icon)
+   // Laitetaan laitokset blokkeihin
+  for (l <- k.laitokset) {
+    asetaLaitos(l)
+  }
+  
+  
+  def getKartta = new Component {
+    
+    minimumSize = new Dimension(2000, 2000)
+    preferredSize = new Dimension(2000, 2000)
+    maximumSize = new Dimension(2000, 2000)
+    
+    override def paintComponent(g: Graphics2D) = {
+    }
+  }
   
   
   def getMini = new scala.swing.BoxPanel(Orientation.Horizontal) {
@@ -79,16 +118,18 @@ class Kartoittaja(k: Kartta) {
   
 }
 
-class Blokki {
+
+class Blokki(val yCoord: Int,val xCoord: Int) {
+
   var laitos: Option[Laitos] = None
   
-  var ikoni: Option[Graphics2D] = ???
+  var ikoni: Option[BufferedImage] = None
   
   var tyhjä = true
   
-  // Hakee myös ikonin
-  def lisääLaitos(l: Laitos) = {
+  def lisääLaitos(l: Laitos, i: BufferedImage) = {
     laitos = Some(l)
+    ikoni = Some(i)
   }
   
 }
